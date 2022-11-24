f230d4cf8fb8a60ddf5796223d17d495f0274422
andeyan <andeyan@qwords.co.id>
2022-11-24 21:36:07 +0700

    add scrollreveal libs

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,38 @@
-console.log('ok ini baru banget pake bs');
+window.sr = ScrollReveal({reset: true})
+
+ScrollReveal().reveal('.main-banner', {
+	cleanup: true,
+	afterReveal: function (el) {
+		ScrollReveal().clean(el)
+	},
+})
+ScrollReveal().reveal('.left-side', {
+	delay: 500,
+	cleanup: true,
+	afterReveal: function (el) {
+		ScrollReveal().clean(el)
+	},
+})
+ScrollReveal().reveal('.right-side', {
+	delay: 1000,
+	interval: 60,
+	afterReveal: function (el) {
+		ScrollReveal().clean(el)
+	},
+})
+ScrollReveal().reveal('.card-service', {
+	duration: 1000,
+	scale: 0.5,
+	interval: 90,
+	afterReveal: function (el) {
+		ScrollReveal().clean(el)
+	},
+})
+ScrollReveal().reveal('.vision', {
+	origin: 'left',
+	duration: 1000,
+})
+ScrollReveal().reveal('.products .card', {
+	duration: 1000,
+	interval: 50,
+})
